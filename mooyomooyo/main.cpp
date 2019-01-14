c5da126079f14952dfedb16aab4fd539622052a7
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-01-13 21:05:49 -0800

    New rewrite

--- mooyomooyo/main.cpp
+++ mooyomooyo/main.cpp
@@ -12,6 +12,7 @@ LANG: C++14
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <array>
 using namespace std;
 
 vector<string> split(string str, string character){
@@ -32,49 +33,17 @@ vector<string> split(string str, string character){
     return result; 
 }
 
-struct Point{
-    int time;
-    int id;
+struct Block{
+    int color;
+    bool seen;
 };
 
-struct comp {
-  bool operator()( Point a, Point b ) const { 
-    return a.time < b.time;
-  }
-};
+vector<array<int,2>> stack;
+int flood(int x,int y){
 
-vector<vector<int>> grid;
-vector<vector<int>> items;
+}
 
-/*
-int flood(int x=0,int y=0,int px=-1,int py=-1){
-    //returns number of matches, from inputs of x and y coordinates
-    int color=grid[x][y];
-    items.push_back({x,y});
-    cout << x << " " << y << " " << px << " " << py << endl;
-    if(x<9&&grid[x+1][y]==color){
-        if(x+1!=px||y!=py){
-            flood(x+1,y,x,y);
-        }
-    }
-    if(y<grid.size()-1&&grid[x][y+1]==color){
-        if(x!=px||y+1!=py){
-            flood(x,y+1,x,y);
-        }
-    }
-    if(x>1&&grid[x-1][y]==color){
-        if(x-1!=px||y!=py){
-            flood(x-1,y,x,y);
-        }
-    }
-    if(y>1&&grid[x][y-1]==color){
-        if(x!=px||y-1!=py){
-            flood(x,y-1,x,y);
-        }
-    }
-    return 0;
-}*/
-
+vector<vector<Block>> grid;
 
 int main() {
     ofstream fout ("mooyomooyo.out");
@@ -87,12 +56,15 @@ int main() {
     for(int i=1;i<inputstrings.size();i++){
         grid.push_back({});
         for(int k=0;k<10;k++){
-            grid[i-1].push_back(stoi(string(1,inputstrings[i][k])));
+            Block thing=Block();
+            grid[i-1].push_back(thing);
         }
     }
     for(int i=0;i<grid.size();i++){
         for(int k=0;k<grid[i].size();k++){
-//            flood(i,k);
+            if(grid[i][k].seen==false){
+                flood(i,k);
+            }
         }
     }
     return 0;
