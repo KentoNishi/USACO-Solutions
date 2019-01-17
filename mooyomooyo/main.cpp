2ff0d6ee1cae57690e823130da4fe4ec9c400e19
Kento Nishi <kento24gs@outlook.com>
2019-01-17 15:20:38 -0800

    Spacing

--- mooyomooyo/main.cpp
+++ mooyomooyo/main.cpp
@@ -72,7 +72,7 @@ int gravity(){
     for(int x=0;x<10;x++){
         int bottom=grid.size()-1;
         for(int y=grid.size()-1;y>=0;y--){
-            if(grid[y][x].color!=0 && y<bottom&&grid[bottom][x].color==0){
+            if(grid[y][x].color!=0 && y<bottom && grid[bottom][x].color==0){
                 grid[bottom][x].color=grid[y][x].color;
                 grid[y][x].color=0;
                 bottom--;
