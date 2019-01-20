bef76946819a3f789d512a9cb45f8474620b181e
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-01-20 15:45:43 -0800

    Teleport - too difficult :(

--- teleport/main.cpp
+++ teleport/main.cpp
@@ -34,6 +34,7 @@ vector<string> split(string str, string character){
 }
 
 int  main() {
+    //Too difficult, can't solve or understand at all
     ofstream fout ("teleport.out");
     ifstream fin ("teleport.in");
     vector<string> inputstrings;
