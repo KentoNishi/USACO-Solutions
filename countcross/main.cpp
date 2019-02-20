6013736883b794155bf0e56ad5d9324762ce03d2
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-02-19 19:24:31 -0800

    Gave up CountCross

--- countcross/main.cpp
+++ countcross/main.cpp
@@ -13,9 +13,6 @@ LANG: C++14
   - -
 # X|X
 */
-//for each combination
-    //fill to find item
-    //if not found increase counter
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -49,56 +46,75 @@ struct comp {
     }
 };
 
-class Coordinate{
+class Coord{
     public:
         int x;
         int y;
+        Coord(int px, int py){
+            x=px;
+            y=py;
+        }
 };
 
 class Field{
     public:
-        vector<Coordinate> notFrom;
-        bool find(int x,int y){
-            for(int i=0;i<notFrom.size();i++){
-                if(notFrom[i].x==x&&notFrom[i].y==y){
-                    return true;
-                }
-            }
-            return false;
-        }
+        vector<Coord> walls;
         bool cow=false;
         bool seen=false;
+        int id=-1;
+        bool validate(int x, int y){
+            for(int i=0;i<walls.size();i++){
+                if(walls[i].x==x&&walls[i].y==y){
+                    return false;
+                }
+            }
+            return true;
+        }
 };
 
 vector<vector<Field>> grid;
+vector<vector<Field>> original;
 int counter=0;
-void floodFill(int x, int y,int px,int py){
+//vector<pair<Coord,Coord>> pairs; 
+void flood(int x, int y,int pid){
     if(x<0||y<0||y>=grid.size()||x>=grid.size()){
         return;
     }
-    if(grid[y][x].seen!=false){
+    if(grid[y][x].seen==true){
         return;
     }
+ //   cout << x << ", " << y << "   " <<" = "<<grid[y][x].cow<< endl;
+    if(grid[y][x].cow==true&&grid[y][x].id<pid){
+        counter++;
+//        cout << x <<", "<<y << endl;
+    }
     grid[y][x].seen=true;
-    if((x==px&&y==py)==false&&grid[y][x].cow==true){
-        if(x>=px&&y>=py){
-            counter++;
-        }
+    if(x>=1&&grid[y][x-1].validate(x,y)){
+        flood(x-1,y,pid);
     }
-    if(x<grid.size()-1&&grid[y][x+1].find(x,y)==false){
-        floodFill(x+1,y,px,py);
+    if(y>=1&&grid[y-1][x].validate(x,y)){
+        flood(x,y-1,pid);
     }
-    if(x>0&&grid[y][x-1].find(x,y)==false){
-        floodFill(x-1,y,px,py);
+    if(y<grid.size()-1&&grid[y+1][x].validate(x,y)){
+        flood(x,y+1,pid);
     }
-    if(y<grid.size()-1&&grid[y+1][x].find(x,y)==false){
-        floodFill(x,y+1,px,py);
+    if(x<grid.size()-1&&grid[y][x+1].validate(x,y)){
+        flood(x+1,y,pid);
     }
-    if(y>0&&grid[y-1][x].find(x,y)==false){
-        floodFill(x,y-1,px,py);
-    }
-    return;
 }
+
+int fact(int n) 
+{ 
+    int res = 1; 
+    for (int i = 2; i <= n; i++) 
+        res = res * i; 
+    return res; 
+} 
+
+int nCr(int n, int r) 
+{ 
+    return fact(n) / (fact(r) * fact(n - r)); 
+} 
 
 int main() {
     ofstream fout ("countcross.out");
@@ -109,49 +125,61 @@ int main() {
         inputstrings.push_back(contents);
     }
     vector<string> firstLine=split(inputstrings[0]," ");
-    for(int i=0;i<stoi(firstLine[0]);i++){
+    for(int y=0;y<stoi(firstLine[0]);y++){
         grid.push_back({});
-        for(int k=0;k<stoi(firstLine[0]);k++){
+        for(int x=0;x<stoi(firstLine[0]);x++){
             Field field=Field();
-            grid[i].push_back(field);
+            grid[y].push_back(field);
         }
     }
     for(int i=1;i<=stoi(firstLine[1]);i++){
         vector<string> splitln=split(inputstrings[i]," ");
-        Coordinate coord1=Coordinate();
-        coord1.x=stoi(splitln[0])-1;
-        coord1.y=stoi(splitln[1])-1;
-        Coordinate coord2=Coordinate();
-        coord2.x=stoi(splitln[2])-1;
-        coord2.y=stoi(splitln[3])-1;
-        grid[coord1.y][coord1.x].notFrom.push_back(coord2);
-        grid[coord2.y][coord2.x].notFrom.push_back(coord1);
-//        cout << coord1.x << " " << coord1.y << " " << coord2.x << " " << coord2.y << endl;
+        Coord c1=Coord(stoi(splitln[0])-1,stoi(splitln[1])-1);
+        Coord c2=Coord(stoi(splitln[2])-1,stoi(splitln[3])-1);
+        grid[c1.y][c1.x].walls.push_back(c2);
+        grid[c2.y][c2.x].walls.push_back(c1);
     }
-    vector<Coordinate> cows;
+    vector<Coord> cows;
     for(int i=1+stoi(firstLine[1]);i<inputstrings.size();i++){
         vector<string> splitln=split(inputstrings[i]," ");
-        Coordinate coord=Coordinate();
-        coord.x=stoi(splitln[0])-1;
-        coord.y=stoi(splitln[1])-1;
-        cows.push_back(coord);
-        grid[coord.y][coord.x].cow=true;
+        Coord cow=Coord(stoi(splitln[0])-1,stoi(splitln[1])-1);
+        cows.push_back(cow);
+        grid[cow.y][cow.x].cow=true;
+        grid[cow.y][cow.x].id=i-(1+stoi(firstLine[1]));
     }/*
     for(int y=0;y<grid.size();y++){
         for(int x=0;x<grid.size();x++){
-            cout << grid[y][x].cow << " ";
+            cout << grid[y][x].cow<<"-"<<grid[y][x].walls.size()<<" ";
         }
         cout << endl;
     }*/
-    vector<vector<Field>> original=grid;
-    int mini;
+    original=grid;
+    int ans=0;
     for(int i=0;i<cows.size();i++){
         grid=original;
         counter=0;
-        floodFill(cows[i].x,cows[i].y,cows[i].x,cows[i].y);
-        mini=max(mini,counter);
+//        cout << cows[i].x << ", " << cows[i].y << ":" <<i<< endl;
+        flood(cows[i].x,cows[i].y,i);
+        ans+=counter;
 //        cout << counter << endl;
     }
-    fout << cows.size()-1-mini<< endl;
+    cout << ans << endl;
+//    fout << nCr(cows.size(),2) - ans << endl;
     return 0;
 }
+/*
+# # #
+
+# X #
+  - -
+# X|X
+
+3 3 3
+2 2 2 3
+3 3 3 2
+3 3 2 3
+3 3
+2 2
+2 3
+*/
+
