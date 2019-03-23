b382ff6d05a86e75b13af1d9d06837d20fb3cc54
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-03-23 14:37:24 -0700

    Not working

--- mootube/main.cpp
+++ mootube/main.cpp
@@ -47,28 +47,29 @@ struct Edge{
 
 struct Node{
     vector<Edge> edges;
-    bool seen=false;
 };
 
-int bfs(int v, int k,array<Node,5001> graph){
+vector<vector<Edge>> edges;/*
+int bfs(int v, int k,bool seen[stoi(firstLine[0])]){
     int ans=0;
     queue<int> queue;
     queue.push(v);
-    graph[v].seen=true;
+    seen[v]=true;
     while(!queue.empty()){
         int id=queue.front();
         queue.pop();
-        for(int i=0;i<graph[id].edges.size();i++){
-            Edge out=graph[id].edges[i];
-            if(graph[out.id].seen!=true&&out.weight>=k){
-                graph[out.id].seen=true;
+        for(int i=0;i<edges.size();i++){
+            Edge out=edges[id][i];
+            cout << out.id << " " << out.weight << " " << k << endl;
+            if(seen[out.id]!=true&&out.weight>=k){
+                seen[out.id]=true;
                 queue.push(out.id);
                 ans++;
             }
         }
     }
     return ans;
-}
+}*/
 
 int main() {
     ofstream fout ("mootube.out");
@@ -78,22 +79,43 @@ int main() {
     while(getline(fin,contents)) {
         inputstrings.push_back(contents);
     }
-//    unordered_map<int,Node> graph;
+//    unordered_map<int,Node> seen;
     vector<string> firstLine=split(inputstrings[0]," ");
-    array<Node,5001> graph;
+    edges=vector<vector<Edge>>(stoi(firstLine[0]));
+    for(int i=0;i<stoi(firstLine[0]);i++){
+        edges[i]={};
+    }
     for(int i=1;i<stoi(firstLine[0]);i++){
         vector<string> splitln=split(inputstrings[i]," ");
         int a=stoi(splitln[0])-1;
         int b=stoi(splitln[1])-1;
         int w=stoi(splitln[2]);
-        graph[a].edges.push_back(Edge(b,w));
-        graph[b].edges.push_back(Edge(a,w));
+        edges[a].push_back(Edge(b,w));
+        edges[b].push_back(Edge(a,w));
     }
     for(int i=stoi(firstLine[0]);i<inputstrings.size();i++){
         vector<string> splitln=split(inputstrings[i]," ");
         int v=stoi(splitln[1])-1;
         int k=stoi(splitln[0]);
-        fout << bfs(v,k,graph) << endl;
+        bool seen[stoi(firstLine[0])]; 
+        int ans=0;
+        queue<int> queue;
+        queue.push(v);
+        seen[v]=true;
+        while(!queue.empty()){
+            int id=queue.front();
+            queue.pop();
+            for(int i=0;i<edges[id].size();i++){
+                Edge out=edges[id][i];
+//                cout << out.id << " " << out.weight << " " << k << endl;
+                if(seen[out.id]!=true&&out.weight>=k){
+                    seen[out.id]=true;
+                    queue.push(out.id);
+                    ans++;
+                }
+            }
+        }
+        fout << ans << endl;
     }
     return 0;
 }
