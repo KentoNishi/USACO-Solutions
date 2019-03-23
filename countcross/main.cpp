c6f405613c05fad40a0a844a73f2e9d91f4dd39a
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-03-23 11:30:18 -0700

    CountCross not working

--- countcross/main.cpp
+++ countcross/main.cpp
@@ -16,6 +16,7 @@ LANG: C++14
 #include <algorithm>
 #include <unordered_map>
 #include <array>
+#include <map>
 using namespace std;
 
 vector<string> split(string str, string character){
@@ -54,6 +55,25 @@ struct Coord{
     }
 };
 
+vector<vector<vector<Coord>>> walls;
+void dfs(vector<vector<bool>>& reachable,int x, int y){
+    if(x<0||x>=(reachable).size()||y<0||y>=(reachable)[x].size()||(reachable)[x][y]){
+        return;
+    }
+    (reachable)[x][y]=true;
+    vector<pair<int,int>> directions={{-1,0},{0,1},{1,0},{0,-1}};
+    for(int i=0;i<directions.size();i++){
+        int nx=x+directions[i].first;
+        int ny=y+directions[i].second;
+        Coord toFind=Coord(nx,ny);
+        if(find_if(walls[x][y].begin(),walls[x][y].end(),[toFind](Coord const& n ){
+            return (n.x==toFind.x&&n.y==toFind.y);
+        })==walls[x][y].end()){
+            dfs(reachable,nx,ny);
+        }
+    }
+}
+
 int main() {
     ofstream fout ("countcross.out");
     ifstream fin ("countcross.in");
@@ -63,14 +83,44 @@ int main() {
         inputstrings.push_back(contents);
     }
     vector<string> firstLine=split(inputstrings[0]," ");
-    Coord walls[stoi(firstLine[0])][stoi(firstLine[0])];
+    for(int x=0;x<stoi(firstLine[0]);x++){
+        walls.push_back({});
+        for(int y=0;y<stoi(firstLine[0]);y++){
+            walls[x].push_back({});
+        }
+    }
     for(int i=1;i<=stoi(firstLine[1]);i++){
         vector<string> splitln=split(inputstrings[i]," ");
         vector<int> coords;
         for(int k=0;k<splitln.size();k++){
-            coords.push_back(stoi(splitln[k]));
+            coords.push_back(stoi(splitln[k])-1);
         }
-        
+        walls[coords[0]][coords[1]].push_back(Coord(coords[2],coords[3]));
+        walls[coords[2]][coords[3]].push_back(Coord(coords[0],coords[1]));
     }
+    vector<Coord> cows;
+    int ans=0;
+    vector<Coord> coords;
+    for(int i=stoi(firstLine[1])+1;i<inputstrings.size();i++){
+        vector<string> line=split(inputstrings[i]," ");
+        coords.push_back(Coord(stoi(line[0])-1,stoi(line[1])-1));
+    }
+    for(int i=0;i<coords.size();i++){
+        cows.push_back(coords[i]);
+        vector<vector<bool>> reachable;
+        for(int px=0;px<(stoi(firstLine[0]));px++){
+            reachable.push_back({});
+            for(int py=0;py<(stoi(firstLine[0]));py++){
+                reachable[px].push_back(false);
+            }
+        }
+        dfs(reachable,coords[i].x,coords[i].y);
+        for(int j=0;j<i;j++){
+            if(reachable[cows[j].x][cows[j].y]!=true){
+                ans++;
+            }
+        }
+    }
+    fout << ans << endl;
     return 0;
 }
