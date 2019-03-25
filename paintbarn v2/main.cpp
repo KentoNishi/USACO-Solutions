95b302cb57d0ca7252fd4cf3b4592017b5170703
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-03-25 14:35:23 -0700

    Started paintbarn v2

---
+++ paintbarn v2/main.cpp
@@ -0,0 +1,51 @@
+//http://usaco.org/index.php?page=viewproblem2&cpid=918
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <bitset>
+#include <tuple>
+#include <cmath>
+using namespace std;
+
+vector<string> split(string str, string character){
+    vector<string> result;
+    long long s=0;
+    long long i=0;
+    while(i<str.length()){
+        if(str[i]==character[0]||i==str.length()-1){
+            long long x=i-s;
+            if(i==str.length()-1){
+                x++;
+            }
+            result.push_back(str.substr(s,x));
+            s=i+1;
+        }
+        i++;
+    }
+    return result;
+}
+
+int grid[1001][1001];
+int main() {
+    ofstream fout ("paintbarn.out");
+    ifstream fin ("paintbarn.in");
+    vector<string> inputstrings;
+    string contents;
+    while(getline(fin,contents)) {
+        inputstrings.push_back(contents);
+    }
+    vector<string> firstLine=split(inputstrings[0]," ");
+    int match=stoi(firstLine[1]);
+    for(int i=1;i<inputstrings.size();i++){
+        vector<string> line=split(inputstrings[i]," ");
+        int a=stoi(line[0]);
+        int b=stoi(line[1]);
+        int c=stoi(line[2]);
+        int d=stoi(line[3]);
+        grid[a][b]++;
+    }
+	return 0;
+}
