86762e717f29f0da8fbe7b1d2fc16757f2c96b90
Kento Nishi <kento24gs@outlook.com>
2019-11-15 10:59:47 -0800

    Sadly still unfinished :cry:

--- 2019-2020/training/2017-2018/february/silver/teleport/main.cpp
+++ 2019-2020/training/2017-2018/february/silver/teleport/main.cpp
@@ -10,6 +10,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void addPoint(map<long long, long long> &map, long long point, long long num) {
+
+    if (map.count(point) > 0) {
+        map[point] += num;
+    }
+    map[point] = num;
+}
+
 int main() {
     ofstream fout("teleport.out");
     ifstream fin("teleport.in");
@@ -18,68 +26,56 @@ int main() {
     vector<pair<long long, long long>> pairs = vector<pair<long long, long long>>(N);
     // location1, location2
     // location1 < location2
+    long long currentDist = 0;
     for (long long i = 0; i < N; i++) {
         fin >> pairs[i].first >> pairs[i].second;
         cout << pairs[i].first << " " << pairs[i].second << endl;
         if (pairs[i].first > pairs[i].second) {
             swap(pairs[i].first, pairs[i].second);
         }
-    }
-    vector<pair<long long, long long>> criticalPoints;
-    // location, slopeChange
-    for (long long i = 0; i < N; i++) {
-        if (pairs[i].second - pairs[i].first <= min(abs(pairs[i].first), abs(pairs[i].second))) {
-            // always shorter to just go directly
-            continue;
-        }
-        if (pairs[i].first == 0) {
-            // left side of boundary is at portal
-            criticalPoints.push_back(make_pair(0, -1));
-            criticalPoints.push_back(make_pair(pairs[i].second, 2));
-            criticalPoints.push_back(make_pair(pairs[i].second * 2, -1));
-        }
-        if (pairs[i].second == 0) {
-            // right side of boundary is at portal
-            criticalPoints.push_back(make_pair(pairs[i].first * 2, -1));
-            criticalPoints.push_back(make_pair(pairs[i].first, 2));
-            criticalPoints.push_back(make_pair(0, -1));
-        }
-        if (pairs[i].first > 0 && pairs[i].second > 0) {
-            // both points more than 0
-            criticalPoints.push_back(make_pair(pairs[i].first * 2, -1));
-            criticalPoints.push_back(make_pair(pairs[i].second, 2));
-            criticalPoints.push_back(make_pair(2 * pairs[i].second - 2 * pairs[i].first, -1));
-        }
-        if (pairs[i].first < 0 && pairs[i].second < 0) {
-            // both points less than 0
-            criticalPoints.push_back(make_pair(2 * pairs[i].first - 2 * pairs[i].second, -1));
-            criticalPoints.push_back(make_pair(pairs[i].second, 2));
-            criticalPoints.push_back(make_pair(pairs[i].first * 2, -1));
-        }
-        if (pairs[i].first < 0 && pairs[i].second > 0) {
-            // on both sides of 0
-            criticalPoints.push_back(make_pair(2 * pairs[i].first, -1));
-            criticalPoints.push_back(make_pair(pairs[i].first, 2));
-            criticalPoints.push_back(make_pair(0, -2));
-            criticalPoints.push_back(make_pair(pairs[i].second, 2));
-            criticalPoints.push_back(make_pair(2 * pairs[i].second, -1));
-        }
-    }
-    sort(criticalPoints.begin(), criticalPoints.end());
-    long long currentDist = 0;
-    for (long long i = 0; i < N; i++) {
         currentDist += pairs[i].second - pairs[i].first;
     }
-    long long minDist = currentDist;
-    long long previousLocation = 0;
-    long long slope = 0;
-    //cout << criticalPoints.size()
+    map<long long, long long> map;
+    for (long long i = 0; i < N; i++) {
+        if (pairs[i].second - pairs[i].first <= min(abs(pairs[i].first), abs(pairs[i].second))) {
+            // if the shortest path is to go directly
+            continue;
+            // skip this pair
+        }
+        //    *--|------*
+        if (pairs[i].first < 0 && pairs[i].second > 0) {
+            // left side less than 0
+            // right side more than 0
+            addPoint(map, pairs[i].first * 2, -1);
+            addPoint(map, pairs[i].first, 2);
+            addPoint(map, 0, -2);
+            addPoint(map, pairs[i].second, 2);
+            addPoint(map, pairs[i].second * 2, -1);
+        }
+        //     *---*   |
+        // |    *---*
+        if (pairs[i].second < 0 || pairs[i].first > 0) {
+            // both below 0 or both above 0
+            addPoint(map, pairs[i].first * 2 - pairs[i].second, -1);
+            addPoint(map, pairs[i].first, 2);
+            addPoint(map, pairs[i].second, -1);
+        }
+    }
+    vector<pair<int, int>> criticalPoints;
+    for (auto &elem : map) {
+        criticalPoints.push_back(make_pair(elem.first, elem.second));
+    }
     for (long long i = 0; i < criticalPoints.size(); i++) {
-        currentDist += (criticalPoints[i].first - previousLocation) * slope;
-        previousLocation = criticalPoints[i].first;
-        minDist = min(minDist, currentDist);
-        cout << currentDist << " @ location " << criticalPoints[i].first << endl;
+        cout << "Point at " << criticalPoints[i].first << " " << criticalPoints[i].second << endl;
+    }
+    long long minDist = currentDist;
+    long long previous = 0;
+    long long slope = 0;
+    for (long long i = 0; i < criticalPoints.size(); i++) {
+        currentDist += slope * (criticalPoints[i].first - previous);
+        previous = criticalPoints[i].first;
         slope += criticalPoints[i].second;
+        minDist = min(minDist, currentDist);
     }
     fout << minDist << endl;
     return 0;
