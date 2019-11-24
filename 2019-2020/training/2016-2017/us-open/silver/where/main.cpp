0bed8d2fb3c499d5691120f0ab60ff1e40b4f669
Kento Nishi <kento24gs@outlook.com>
2019-11-24 12:25:54 -0800

    Still broken :(

--- 2019-2020/training/2016-2017/us-open/silver/where/main.cpp
+++ 2019-2020/training/2016-2017/us-open/silver/where/main.cpp
@@ -20,7 +20,7 @@ struct Region {
 
 vector<vector<Pixel>> grid;
 
-void markRegions(int x, int y, int value, int region) {
+void markRegions(int x, int y, char value, int region) {
     if (x < 0 || y < 0 || x >= N || y >= N) {
         return;
     }
@@ -36,12 +36,18 @@ void markRegions(int x, int y, int value, int region) {
     markRegions(x - 1, y, value, region);
     markRegions(x, y + 1, value, region);
     markRegions(x, y - 1, value, region);
-    /*
-    regions[region].maxX = max(regions[region].maxX, x);
-    regions[region].maxY = max(regions[region].maxY, y);
-    regions[region].minX = min(regions[region].minX, x);
-    regions[region].minY = min(regions[region].minY, y);*/
     return;
+}
+
+int inPCL(int xB, int yB, int xS, int yS, vector<Region> selected, int id) {
+    for (int i = 0; i < selected.size(); i++) {
+        auto r = selected[i];
+        if (id != i && r.maxX >= xB && r.maxY >= yB && r.minX <= xS && r.minY <= yS) {
+            //cout << r.minX << " " << r.minY << " " << r.maxX << " " << r.maxY << " was a match" << endl;
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main() {
@@ -65,96 +71,54 @@ int main() {
                 region++;
             }
         }
-    }
+    } /*
     for (int x = 0; x < N; x++) {
         for (int y = 0; y < N; y++) {
             cout << grid[x][y].region << " ";
         }
         cout << endl;
-    }
+    }*/
 
     int ans = 0;
     vector<Region> selected;
-    for (int x1 = N - 1; x1 >= 0; x1--) {
-        for (int y1 = N - 1; y1 >= 0; y1--) {
-            for (int x2 = 0; x2 <= x1; x2++) {
-                for (int y2 = 0; y2 <= y1; y2++) {
-                    cout << "Coord: " << x2 << " " << y2 << " " << x1 << " " << y1 << endl;
+    for (int xS = 0; xS < N; xS++) {
+        for (int yS = 0; yS < N; yS++) {
+            for (int xB = xS; xB < N; xB++) {
+                for (int yB = yS; yB < N; yB++) {
                     map<char, set<int>> counts;
-                    // pixel, region ids
-                    bool exit = false;
-                    for (auto &reg : selected) {
-                        // mins: x2,y2
-                        // maxs: x1, y1
-                        if (x2 >= reg.minX && x1 <= reg.maxX && y2 >= reg.minY && y1 <= reg.maxY) {
-                            exit = true;
-                            break;
+                    for (int x = xS; x <= xB; x++) {
+                        for (int y = yS; y <= yB; y++) {
+                            counts[grid[x][y].value].insert(grid[x][y].region);
                         }
                     }
-                    if (exit) {
-                        continue;
-                    }
-                    for (int x3 = x2; x3 <= x1; x3++) {
-                        for (int y3 = y2; y3 <= y1; y3++) {
-                            counts[grid[x3][y3].value].insert(grid[x3][y3].region);
-                        }
-                    } /*
-                    if(x2==0 && y2==2 && x1==3 && y1==3){
-                        for(auto &m:counts){
-                            cout << m.first << ": " << m.second.size() << endl;
-                        }
-                    }*/
                     if (counts.size() == 2) {
                         auto p = counts.begin();
                         int a = (*p).second.size();
                         p++;
                         int b = (*p).second.size();
-                        if (a > b) {
-                            swap(a, b);
-                        }
-                        if (a == 1 && b >= 2) { /*
-                            cout << "!!! Choosing " << x2 << " " << y2 << " " << x1 << " " << y1 << endl;
-                            for (auto &reg : selected) {
-                                cout << "already selected: " << reg.minX << " " << reg.minY << " " << reg.maxX << " " << reg.maxY << endl;
-                            }*/
+                        if (a == 1 && b >= 2 || b == 1 && a >= 2) {
+                            //cout << "!!! Choosing " << xS << " " << yS << " " << xB << " " << yB << endl;
                             selected.push_back(Region());
-                            selected[selected.size() - 1].maxX = x1;
-                            selected[selected.size() - 1].maxY = y1;
-                            selected[selected.size() - 1].minX = x2;
-                            selected[selected.size() - 1].minY = y2;
-                            ans++;
+                            selected[selected.size() - 1].maxX = xB;
+                            selected[selected.size() - 1].maxY = yB;
+                            selected[selected.size() - 1].minX = xS;
+                            selected[selected.size() - 1].minY = yS;
                         }
                     }
                 }
             }
         }
     }
+    for (int i = 0; i < selected.size(); i++) {
+        auto r = selected[i];
+        //cout << "selected: " << r.minX << " " << r.minY << " " << r.maxX << " " << r.maxY << endl;
+        int res = inPCL(r.maxX, r.maxY, r.minX, r.minY, selected, i);
+        if (res == -1) {
+            cout << r.minX << " " << r.minY << " " << r.maxX << " " << r.maxY << " is final" << endl;
+            ans++;
+        } else {
+        }
+    }
     fout << ans << endl;
-
-    /*
-    for (int i = 0; i < region; i++) {
-        cout << "region " << i << " has bounds " << regions[i].minX << " " << regions[i].minY << " " << regions[i].maxX << " " << regions[i].maxY << endl;
-        map<char,set<int>> counts;
-        // pixel value,regions of pixel value
-        for(int x=regions[i].minX;x<=regions[i].maxX;x++){
-            for(int y=regions[i].minY;y<=regions[i].maxY;y++){
-                cout << "Investigating region " << i << " @ " << x << " " << y << endl;
-                counts[grid[x][y].value].insert(i);
-            }
-        }
-        cout << "resulted in " << counts.size() << endl;
-        if(counts.size()==2){
-            auto p=counts.begin();
-            int a=(*p).second.size();
-            p++;
-            int b=(*p).second.size();
-            if(a>b){
-                swap(a,b);
-            }
-            if(a==1&&b>=2){
-                ans++;
-            }
-        }
-    }*/
     return 0;
 }
