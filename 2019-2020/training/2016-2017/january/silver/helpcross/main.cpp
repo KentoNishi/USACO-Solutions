839f4707d91975e7026953f8779b70daff345ce1
Kento Nishi <kento24gs@outlook.com>
2019-11-26 14:19:41 -0800

    Finished helpcross - Silver - January 2016-2017
    http://usaco.org/index.php?page=viewproblem2&cpid=714

--- 2019-2020/training/2016-2017/january/silver/helpcross/main.cpp
+++ 2019-2020/training/2016-2017/january/silver/helpcross/main.cpp
@@ -4,23 +4,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int C,N;
+int C, N;
 vector<int> chickens;
-vector<pair<int,int>> cows;
-vector<vector<int>> pairs;
+vector<pair<int, int>> cows;
+struct compare {
+    bool operator()(pair<int, int> a, pair<int, int> b) const {
+        return a.second < b.second;
+    }
+};
 
 int main() {
     ifstream fin("helpcross.in");
     ofstream fout("helpcross.out");
     fin >> C >> N;
-    chickens=vector<int>(C);
-    for(int i=0;i<C;i++){
+    chickens = vector<int>(C);
+    for (int i = 0; i < C; i++) {
         fin >> chickens[i];
     }
-    cows=vector<pair<int,int>>(N);
-    for(int i=0;i<N;i++){
+    cows = vector<pair<int, int>>(N);
+    for (int i = 0; i < N; i++) {
         fin >> cows[i].first >> cows[i].second;
     }
-
+    sort(chickens.begin(), chickens.end());
+    sort(cows.begin(), cows.end(), compare());
+    int ans = 0;
+    for (int i = 0; i < cows.size(); i++) {
+        auto chicken = lower_bound(chickens.begin(), chickens.end(), cows[i].first);
+        if (chicken != chickens.end() && *chicken <= cows[i].second) {
+            ans++;
+            chickens.erase(chicken);
+        }
+    }
+    fout << ans << endl;
     return 0;
 }
