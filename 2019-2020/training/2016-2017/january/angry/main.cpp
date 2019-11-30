4ebc6dec4152cb7ed764df3f2885cf641bd0ab0c
Kento Nishi <kento24gs@outlook.com>
2019-11-29 22:07:03 -0800

    Finished angry - Silver - January 2016-2017
    http://usaco.org/index.php?page=viewproblem2&cpid=594

---
+++ 2019-2020/training/2016-2017/january/angry/main.cpp
@@ -0,0 +1,51 @@
+// Test case path: [path]
+// angry - Silver - January 2016-2017
+// http://usaco.org/index.php?page=viewproblem2&cpid=594
+
+#include <bits/stdc++.h>
+using namespace std;
+int N, K;
+vector<int> blocks;
+
+bool possible(int radius) {
+    int diameter = 2 * radius;
+    int cowID = 0;
+    int blockID = 0;
+    int firstInRadius = blocks[0];
+    while (cowID < K && blockID < N) {
+        if (blocks[blockID] - firstInRadius > diameter) {
+            cowID++;
+            firstInRadius = blocks[blockID];
+            continue;
+        }
+        blockID++;
+    }
+    return blockID == N && cowID < K;
+}
+
+int main() {
+    ifstream fin("angry.in");
+    ofstream fout("angry.out");
+    fin >> N >> K;
+    blocks = vector<int>(N);
+    for (int i = 0; i < N; i++) {
+        fin >> blocks[i];
+    }
+    sort(blocks.begin(), blocks.end());
+    int right = blocks[N - 1] - blocks[0];
+    int left = 0;
+    while (right > left && right - left > 1) {
+        int test = (right + left) / 2.0;
+        if (possible(test)) {
+            right = test;
+        } else {
+            left = test;
+        }
+    }
+    if (possible(left)) {
+        fout << left << endl;
+    } else {
+        fout << right << endl;
+    }
+    return 0;
+}
