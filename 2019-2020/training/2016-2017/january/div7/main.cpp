7fc7a89cd4d0d47d5ec733ad847f1c2f18ea456c
Kento Nishi <kento24gs@outlook.com>
2019-12-05 12:39:46 -0800

    Created d7 files

---
+++ 2019-2020/training/2016-2017/january/div7/main.cpp
@@ -0,0 +1,12 @@
+// Test case path: [path]
+// dev7 - Division - Month Season
+// url
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("dev7.in");
+    ofstream fout("dev7.out");
+    return 0;
+}
