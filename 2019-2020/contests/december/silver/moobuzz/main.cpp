3b3f335f3384295bb56b30684d0f20a167ca56ce
Kento Nishi <kento24gs@outlook.com>
2019-12-19 18:56:55 -0800

    Finished moobuzz [13/13] - Silver - December 2019-2020
    Previously 11/13.
    http://usaco.org/index.php?page=viewproblem&cpid=954

--- 2019-2020/contests/december/silver/moobuzz/main.cpp
+++ 2019-2020/contests/december/silver/moobuzz/main.cpp
@@ -1,38 +1,22 @@
 // Test case path: [path]
-// moobuzz - Division - Month Season
+// moobuzz - Silver - December 2019-2020
 // http://usaco.org/index.php?page=viewproblem&cpid=954
 
 #include <bits/stdc++.h>
 using namespace std;
-long long N;
+int N;
 
-long long indexOf(long long m) {
-    return m - (m / 3) - (m / 5) + (m / 15);
-}
-
-long long binarySearch(long long l, long long h) {
-    long long m = (l + h) / 2;
-    long long index = indexOf(m);
-    if (abs(h - l) <= 1) {
-        if (indexOf(h) == N) {
-            return h;
-        } else {
-            return l;
-        }
-    }
-    if (index == N) {
-        return m;
-    } else if (index < N) {
-        return binarySearch(m, h);
-    } else {
-        return binarySearch(l, m);
-    }
-}
+vector<int> first8;
 
 int main() {
     ifstream fin("moobuzz.in");
     ofstream fout("moobuzz.out");
     fin >> N;
-    fout << binarySearch(N - 1, pow(2, 32)) << endl;
+    for (int i = 1; i <= 15; i++) {
+        if (i % 3 > 0 && i % 5 > 0) {
+            first8.push_back(i);
+        }
+    }
+    fout << first8[(N - 1) % 8] + 15 * ((N - 1) / 8) << endl;
     return 0;
 }
