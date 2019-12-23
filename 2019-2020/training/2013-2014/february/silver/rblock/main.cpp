f7ce2746c7622da25ab61fdeaf89eb78c3705c5d
Kento Nishi <kento24gs@outlook.com>
2019-12-22 22:51:24 -0800

    Removed template.in

---
+++ 2019-2020/training/2013-2014/february/silver/rblock/main.cpp
@@ -0,0 +1,12 @@
+// Test case path: [path]
+// rblock - Division - Month Season
+// url
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("rblock.in");
+    ofstream fout("rblock.out");
+    return 0;
+}
