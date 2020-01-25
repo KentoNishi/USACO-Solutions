cde91dc3ea1bb1557ce606b6677f54edbf4b9176
Kento Nishi <kento24gs@outlook.com>
2020-01-24 21:15:08 -0800

    Before reading the official solution

--- 2019-2020/contests/january/silver/berries/main.cpp
+++ 2019-2020/contests/january/silver/berries/main.cpp
@@ -5,13 +5,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printList(vector<int> buckets) {
-    for (int i = 0; i < buckets.size(); i++) {
-        cout << buckets[i] << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     ifstream fin("berries.in");
     ofstream fout("berries.out");
@@ -21,29 +14,33 @@ int main() {
     for (int i = 0; i < N; i++) {
         fin >> trees[i];
     }
-    int ans = 0;
     sort(trees.begin(), trees.end(), greater<int>());
-    for (int i = 2; i <= trees[0]; i++) {
-        vector<int> baskets;
-        for (int j = 0; j < trees.size(); j++) {
-            for (int k = 0; k < trees[j] / i - 1; k++) {
-                baskets.push_back(i);
-            }
-            if (trees[j] >= i) {
-                baskets.push_back(i + trees[j] % i);
-            }
-        }
-        sort(baskets.begin(), baskets.end(), greater<int>());
-        // printList(baskets);
-        if (baskets.size() > K) {
-            continue;
-        }
-        int sum = 0;
-        for (int j = K / 2; j < baskets.size(); j++) {
-            sum += baskets[j];
-        }
-        ans = max(ans, sum);
+    vector<int> baskets = vector<int>(K);
+    for (int i = 0; i < K; i++) {
+        baskets[i] = trees[i];
     }
-    fout << ans << endl;
+    while (baskets[0] / 2 >= baskets[K - 1]) {
+        int split = 3;
+        while (K - split + 1 > 0 && baskets[0] / split >= baskets[K - split + 1]) {
+            split++;
+        }
+        split--;
+        int total = baskets[0];
+        while (total >= 2 * baskets[0] / split) {
+            baskets.push_back(baskets[0] / split);
+            total -= baskets[0] / split;
+        }
+        baskets.push_back(total);
+        baskets.erase(baskets.begin());
+        sort(baskets.begin(), baskets.end(), greater<int>());
+        while (baskets.size() > K) {
+            baskets.erase(baskets.end() - 1);
+        }
+    }
+    int sum = 0;
+    for (int i = K / 2; i < K; i++) {
+        sum += baskets[i];
+    }
+    fout << sum << endl;
     return 0;
 }
