6ed5da6f245cd8a7628961cdf77fa70861931f1c
Kento Nishi <kento24gs@outlook.com>
2020-01-25 21:51:06 -0800

    Finished loan - Silver - January 2019-2020
    http://usaco.org/index.php?page=viewproblem2&cpid=991

--- 2019-2020/contests/january/silver/loan/main.cpp
+++ 2019-2020/contests/january/silver/loan/main.cpp
@@ -1,6 +1,6 @@
 // Test case path: [path]
-// loan - Division - Month Season
-// url
+// loan - Silver - January 2019-2020
+// http://usaco.org/index.php?page=viewproblem2&cpid=991
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,7 +9,7 @@ long long N, K, M;
 
 bool tryIt(long long X) {
     long long given = 0;
-    for (long long day = 0; day < K; day++) {
+    for (long long day = 0; day < K;) {
         long long Y = (N - given) / X;
         if ((N - given) / max(Y, M) > (K - day)) {
             return false;
@@ -18,16 +18,20 @@ bool tryIt(long long X) {
             given += (K - day) * M;
             return given >= N;
         }
-        given += Y;
+        long long howMuchUntilChange = (N - given) % X;
+        if (howMuchUntilChange >= 2 * Y) {
+            long long toGive = Y * (howMuchUntilChange / Y);
+            given += toGive;
+            day += toGive / Y;
+        } else {
+            given += Y;
+            day++;
+        }
         if (given >= N) {
             return false;
         }
     }
-    // cout << given << " for " << X << endl;
-    if (given >= N) {
-        return true;
-    }
-    return false;
+    return given >= N;
 }
 
 int main() {
@@ -36,7 +40,7 @@ int main() {
     fin >> N >> K >> M;
     long long upper = N;
     long long lower = 1;
-    long long ans = N;
+    long long ans = K;
     while (lower < upper - 1) {
         long long avg = (upper + lower) / 2;
         if (tryIt(avg)) {
