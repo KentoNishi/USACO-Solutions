cac84f84ae6cf72e5e29544817e2e3f62748977c
Kento Nishi <kento24gs@outlook.com>
2020-02-20 21:57:42 -0800

    Starting moocast-v2

---
+++ 2019-2020/training/2016-2017/december/silver/moocast-v2/main.cpp
@@ -0,0 +1,12 @@
+// Test case path: [path]
+// moocast - Silver - December 2016-2017
+// http://usaco.org/index.php?page=viewproblem2&cpid=668
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("moocast.in");
+    ofstream fout("moocast.out");
+    return 0;
+}
