2b7190e5b52e4dc4d32036da5c744c95f1c4ec1c
Kento Nishi <kento24gs@outlook.com>
2020-02-21 17:55:49 -0800

    Starting snowboots-v2

---
+++ 2019-2020/training/2017-2018/february/silver/snowboots-v2/main.cpp
@@ -0,0 +1,12 @@
+// Test case path: [path]
+// snowboots - Silver - February 2017-2018
+// http://usaco.org/index.php?page=viewproblem2&cpid=811
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("snowboots.in");
+    ofstream fout("snowboots.out");
+    return 0;
+}
